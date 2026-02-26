14679e3f41dc4de185f3cee4aeb90bb1c0c44515
Aakash Singh <mail@singhaakash.dev>
2026-02-26 22:19:36 +0530

    Update plug_config.py

--- plug_config.py
+++ plug_config.py
@@ -1,6 +1,19 @@
 from plugs.manager import PlugManager
 from plugs.plug import Plug  # noqa: F401
 
-plugs = []
+plugs = [
+    Plug(
+        name="care_state_hmis",
+        package_name="git+https://github.com/10bedicu/care_state_hmis.git",
+        version="@main",
+        configs={},
+    ),
+    Plug(
+        name="abdm",
+        package_name="git+https://github.com/10bedicu/care_abdm.git",
+        version="@production",
+        configs={},
+    )
+]
 
 manager = PlugManager(plugs)
